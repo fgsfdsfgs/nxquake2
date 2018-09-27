6033407df3561cf7dc293299d1e7c9ff25a66939
fgsfds <pvt.fgsfds@gmail.com>
2018-09-28 00:30:23 +0300

    switch: reset context to none before destroying shit

--- src/client/vid/glimp_egl.c
+++ src/client/vid/glimp_egl.c
@@ -147,6 +147,7 @@ DeinitEGL(void)
 {
 	if (s_display)
 	{
+		eglMakeCurrent(s_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
 		if (s_context)
 		{
 			eglDestroyContext(s_display, s_context);
